756155c25e32780cce3d7e227490ac8964b35c62
Imran Khan <imran.khan@kpn.com>
2019-01-17 10:45:49 +0100

    basic routing setup with navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { ProtectedComponent } from './protected/protected.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'contactUs', redirectTo: 'contact' },
+  { path: 'login', component: LoginComponent },
+  { path: 'protected', component: ProtectedComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    LoginComponent,
+    ProtectedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+
+  //  Child module
+  //   ProductsModule
   ],
-  providers: [],
+  providers: [
+  //  Hash routing
+    { provide: LocationStrategy, useClass: HashLocationStrategy}
+  //   AUTH_PROVIDERS,
+  //   LoggedInGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
